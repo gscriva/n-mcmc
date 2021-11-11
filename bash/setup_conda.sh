d1711389d6f3184fea1e5304c4c0ca956837b820
gscriva <giuseppe.scriva@unicam.it>
2021-11-11 18:06:20 +0100

    Update setup conda

--- bash/setup_conda.sh
+++ bash/setup_conda.sh
@@ -13,9 +13,9 @@ source ~/miniconda3/etc/profile.d/conda.sh
 
 # Configure conda env
 read -rp "Enter environment name: " env_name
-read -rp "Enter python version (recommended '3.8') " python_version
-read -rp "Enter cuda version (recommended '10.2', or 'none' for CPU only): " cuda_version
-read -rp "Enter pytorch version (recommended '1.8.1'): " pytorch_version
+read -rp "Enter python version (recommended '3.9') " python_version
+read -rp "Enter cuda version (recommended '11.5', or 'none' for CPU only): " cuda_version
+read -rp "Enter pytorch version (recommended '1.9.1'): " pytorch_version
 
 # Create conda env
 conda create -y -n "$env_name" python="$python_version"
