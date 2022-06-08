734a53269a56d2f9abab8a678a8e41b8083ecd98
gscriva <giuseppe.scriva@unicam.it>
2022-06-08 17:09:53 +0200

    Adjacency use now scipy.sparse

--- src/utils/adjacency.py
+++ src/utils/adjacency.py
@@ -1,27 +1,31 @@
 from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
+from scipy import sparse
 
 
 class Adjacency:
     def __init__(
         self,
         spin_side: int = None,
+        dimensions: int = 2,
         adja_dict: Optional[Dict[tuple, float]] = None,
     ):
         self.AdjaDict: Dict[Tuple(int, int), float] = {}
+        self.Dimensions = dimensions
         self.SpinSide = spin_side
 
         if adja_dict is not None:
             self.AdjaDict = adja_dict
 
         self.NeighboursCouplings: Optional[np.ndarray] = None
-        self.Connectivity: Union[int, List[int]] = None
+        self.Connectivity: Optional[Union[int, List[int]]] = None
         self.MaxNeighbours: Optional[int] = None
         self.AdjaMatrix: Optional[np.ndarray] = None
 
     def create_adjacency(self, connectivity: Union[int, List[int]], seed: int = 12345):
         assert connectivity <= 7, "Not implemented for connectivity greater than 7"
+        assert self.Dimensions == 2, "Random adjacency only implemented for dim=2"
         self.Connectivity = connectivity
         self._create_adjacency(connectivity, seed=seed)
         self._create_neighbours()
@@ -37,20 +41,31 @@ class Adjacency:
     def get_adjamatrix(self) -> np.ndarray:
         if self.AdjaMatrix is None:
             self._create_adjamatrix()
+        return self.AdjaMatrix.toarray()
+
+    def get_sparse(self) -> sparse.coo.coo_matrix:
+        if self.AdjaMatrix is None:
+            self._create_adjamatrix()
         return self.AdjaMatrix
 
-    def savetxt(self):
+    def savetxt(self) -> None:
         assert bool(
             self.AdjaDict
         ), "The connectivity dictionary is empty, instantiate first."
         txtarr = []
         for (i, j), coupling in self.AdjaDict.items():
             # see http://mcsparse.uni-bonn.de/spinglass/ for the format style
+            # see loadtxt as well
             txtarr.append([i + 1, j + 1, coupling])
         np.savetxt(f"couplings-{self.SpinSide}spins", txtarr)
 
-    def loadtxt(self, txt_path):
+    def loadtxt(self, txt_path: str) -> None:
         txt_file = np.loadtxt(txt_path)
+        # see http://spinglass.uni-bonn.de/ for the input format
+        # for 2D lattice the elements are numbered row-wise
+        # for 3D lattice the elements are numbered sequentially
+        # layer by layer starting with index 1
+        # and so on
         adjadict = {}
         for i in range(txt_file.shape[0]):
             adjadict.update(
@@ -58,7 +73,7 @@ class Adjacency:
             )
         self.AdjaDict = adjadict
 
-    def _create_neighbours(self):
+    def _create_neighbours(self) -> None:
         if self.MaxNeighbours is None:
             self.MaxNeighbours = self.SpinSide ** 2
         self.NeighboursCouplings = np.zeros((self.SpinSide ** 2, self.MaxNeighbours, 2))
@@ -73,7 +88,9 @@ class Adjacency:
         self.MaxNeighbours = mask[0]
         self.NeighboursCouplings = np.delete(self.NeighboursCouplings, mask, axis=1)
 
-    def _create_adjacency(self, connectivity: Union[int, List[int]], seed: int = 12345):
+    def _create_adjacency(
+        self, connectivity: Union[int, List[int]], seed: int = 12345
+    ) -> None:
         if isinstance(connectivity, int):
             connectivity = np.arange(connectivity) + 1
         # get the number of neighbours
@@ -96,7 +113,7 @@ class Adjacency:
         spin_num: int,
         connectivity: Union[int, List[int]],
         seed: int,
-    ):
+    ) -> None:
         for connect in connectivity:
             if connect == 1:
                 # right spin coupling
@@ -207,7 +224,11 @@ class Adjacency:
                         }
                     )
 
-    def _create_adjamatrix(self):
-        self.AdjaMatrix = np.zeros((self.SpinSide ** 2, self.SpinSide ** 2))
+    def _create_adjamatrix(self) -> None:
+        i_vec, j_vec = [], []
+        couplings = []
         for (i, j), coupling in self.AdjaDict.items():
-            self.AdjaMatrix[i, j], self.AdjaMatrix[j, i] = coupling, coupling
+            i_vec.extend([i, j])
+            j_vec.extend([j, i])
+            couplings.extend([coupling, coupling])
+        self.AdjaMatrix = sparse.coo_matrix((couplings, (i_vec, j_vec)))
